d6e5d58d344bc3c1f53129ad5e4ce9ce89647930
simabui <sima_sima@live.ru>
2020-02-08 20:04:34 +0200

    render template

--- To-Do/src/js/createNote.js
+++ To-Do/src/js/createNote.js
@@ -3,7 +3,8 @@ const refs = {
   overlay: document.querySelector(".overlay"),
   cancel: document.querySelector("#cancel"),
   save: document.querySelector("#save"),
-  createForm: document.querySelector(".create-form")
+  createForm: document.querySelector(".create-form"),
+  list: document.querySelector("#task-list")
 };
 
 refs.create.addEventListener("click", handleDisplay);
@@ -13,15 +14,20 @@ refs.save.addEventListener("click", handleCreate);
 function handleDisplay(e) {
   e.preventDefault();
 
-  refs.overlay.style.display = "flex";
+  refs.overlay.classList.add("is-open");
 }
 
 function handleCancel(e) {
   e.preventDefault();
   if (e.target === e.currentTarget || e.target === refs.cancel) {
-    refs.overlay.style.display = "none";
+    closeOverlay();
   }
+
   return false;
+}
+
+function closeOverlay() {
+  refs.overlay.classList.remove("is-open");
 }
 
 function handleCreate(e) {
@@ -29,5 +35,20 @@ function handleCreate(e) {
   const input = refs.createForm.elements.title.value;
   const description = refs.createForm.elements.description.value;
   const priority = refs.createForm.priority.value;
-  console.log(input, description, priority);
+  const test = createTemplate(input, description, priority);
+  console.log(input);
+  // if (input || description === "") {
+  //   alert("Fill title and description before Save");
+  //   return;
+  // }
+  refs.list.insertAdjacentHTML("beforeend", test);
+  closeOverlay();
+}
+
+function createTemplate(title, description, priority) {
+  return `<div class="note">
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <div>${priority}</div>
+  </div>`;
 }
